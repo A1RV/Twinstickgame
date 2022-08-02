b604bcd39f61298df4ef745f438e3d1cbecea1a4
Hervé Tran <hervetran28@gmail.com>
2022-08-02 14:14:19 +0200

    Dernière version du module en classe

--- Assets/PremierScript.cs
+++ Assets/PremierScript.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PremierScript : MonoBehaviour
 {
+    public float Speed = 10f;
+    public float rotationSpeed = 100.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Bonjour !");
     }
 
     // Update is called once per frame
@@ -17,13 +19,37 @@ public class PremierScript : MonoBehaviour
 
         //Translation
         //gameObject.transform.Translate(new Vector3(0,0,0.01f));
-
-        float translation = Input.GetAxis("Vertical") * speed;
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
-
-        if (Input.GetAxis("Horizontal"))
+       
+        
+        if (Input.GetKey(KeyCode.UpArrow)) 
         {
-            
+             transform.Translate(Vector3.forward * Time.deltaTime * Speed);
         }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(-Vector3.forward * Time.deltaTime * Speed);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * Speed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * Speed);
+        }
+        
+            float translation = Input.GetAxis("Vertical") * Speed;
+            float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+            translation *= Time.deltaTime;
+            rotation *= Time.deltaTime;
+
+            // Move translation along the object's z-axis
+            transform.Translate(0, 0, translation);
+
+            // Rotate around our y-axis
+            transform.Rotate(0, rotation, 0);
+        
+
     }
 }
